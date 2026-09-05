7ce1e1868c59bc7713c50c01471709d8a1fe7e27
kragua <heldoreiK@gmail.com>
2026-09-05 18:23:05 +0500

    v2.2 Bug fix

--- PasswordAuth.sh
+++ PasswordAuth.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+YELLOW=$'\033[1;33m'
+CYAN=$'\033[0;36m'
+WHITE=$'\033[1;37m'
+BOLD=$'\033[1m'
+NC=$'\033[0m'
+
 source ./Ui.sh
 
 
 if [[ "$PasswordAuth" == "yes" ]]; then
     PasswordAuthStatus="${RED}ON${NC}"
+    PasswordAuthStatusturn="${GREEN}OFF${NC}"
 else
     PasswordAuthStatus="${GREEN}OFF${NC}"
+    PasswordAuthStatusturn="${RED}ON${NC}"
 fi
